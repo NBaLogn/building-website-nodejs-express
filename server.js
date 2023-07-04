8c8cddbab7e59f9c1ff5d75f84c8bc99bbd6d51d
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-07-04 10:58:55 -0400

    index ejs

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.set('view engine','ejs')
 app.set('views',path.join(__dirname, './views'))
 
 app.use(express.static(path.join(__dirname, './static/')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'));
+  res.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (req, res) => {
